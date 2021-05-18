01042f6b2cd22fe3969462ddfe92952f0d6d1be5
thiagoribb <thiribeiro142@gmail.com>
2021-05-18 17:14:12 -0300

    Preparando review

--- src/components/Footer.js
+++
@@ -1,8 +0,0 @@
-export default function Footer({movieSelected}) {
-    return(
-        <div className='footer'>
-            <img src={movieSelected.posterURL}/>
-            <p>{movieSelected.title}</p>
-        </div>
-    )
-}
--- src/components/Seat.js
+++
@@ -1,7 +0,0 @@
-export default function Seat({seat, toggleSelection}) {
-    return(
-        <div className={!seat.isAvailable ? 'seat taken' : seat.isChosen ? 'seat chosen' : 'seat'} seat={seat} onClick={() => toggleSelection(seat)}>
-            <p>{seat.name}</p>
-        </div> 
-    )
-}
